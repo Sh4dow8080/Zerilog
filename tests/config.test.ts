c9e047a6d0729807b1ee500c10c0f852aaf0dbff
Sh4dow8080 <sh4dowpp@gmail.com>
2024-02-24 15:31:46 +0100

    Updated tests to use jest.

--- tests/config.test.ts
+++ tests/config.test.ts
@@ -1,45 +1,30 @@
-import { LoggerConfiguration } from '../src/LoggerConfiguration';
-import { ConsoleZink } from './../src/ConsoleZink';
+import { LoggerConfiguration } from "../src/LoggerConfiguration";
+import { ConsoleZink } from "./../src/ConsoleZink";
 
-import * as chai from 'chai';
-import { LogLevel } from '../src';
+import { LogLevel } from "../src";
 
 describe("building zerilog config", () => {
-    const config = new LoggerConfiguration()
-        .WriteTo(new ConsoleZink())
-        .MinimumLevel.Debug()
-        .Enrich.WithProperty("test", "test")
-        .Enrich.WithProperty("test2", "test2");
+	const config = new LoggerConfiguration()
+		.WriteTo(new ConsoleZink())
+		.MinimumLevel.Debug()
+		.Enrich.WithProperty("test", "test")
+		.Enrich.WithProperty("test2", "test2");
 
-    it("should be able to build config", () => {
-        const logger = config.CreateLogger();
+	it("should be able to build config", () => {
+		const logger = config.CreateLogger();
 
-        chai.expect(logger)
-            .to
-            .not
-            .be
-            .null;
-    });
+		expect(logger).not.toBeNull();
+	});
 
-    it("should have minimum level of debug", () => {
-        chai.expect(config["_minimumLevel"])
-            .to
-            .equal(LogLevel.Debug);
-    });
+	it("should have minimum level of debug", () => {
+		expect(config["_minimumLevel"]).toEqual(LogLevel.Debug);
+	});
 
-    it("should have property of test", () => {
-        chai.expect(config["_context"])
-            .to
-            .have
-            .any
-            .keys("test");
-    });
+	it("should have property of test", () => {
+		expect(config["_context"]).toHaveProperty("test");
+	});
 
-    it("should have property of test2", () => {
-        chai.expect(config["_context"])
-            .to
-            .have
-            .any
-            .keys("test2");
-    });
+	it("should have property of test2", () => {
+		expect(config["_context"]).toHaveProperty("test2");
+	});
 });
--- tsup.config.ts
+++ tsup.config.ts
@@ -9,5 +9,4 @@ export default defineConfig({
 	clean: true,
 	minify: true,
 	treeshake: true,
-	
 });
